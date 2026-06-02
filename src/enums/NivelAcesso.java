75044a4fb31c28958e2d39b019a331c0813f8dad
Elisio-Ferreira <elisio.ferreira.dev@gmail.com>
2026-06-01 22:07:28 -0300

    adicionando os arquivos de enums

--- src/enums/NivelAcesso.java
+++ src/enums/NivelAcesso.java
@@ -1,4 +1,23 @@
 package enums;
 
 public enum NivelAcesso {
+    ADMIN("Administrador"),
+    RH("Recursos Humanos"),
+    GERENTE("Gerente"),
+    FUNCIONARIO("Funcionário");
+
+    private final String descricao;
+
+    NivelAcesso(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    @Override
+    public String toString() {
+        return descricao;
+    }
 }
--- src/enums/TipoOcorrencia.java
+++ src/enums/TipoOcorrencia.java
@@ -1,4 +1,25 @@
 package enums;
 
 public enum TipoOcorrencia {
+    PRESENTE("Presente"),
+    FALTA("Falta"),
+    FALTA_JUSTIFICADA("Falta Justificada"),
+    HORA_EXTRA("Hora Extra"),
+    ATESTADO("Atestado Médico"),
+    FOLGA("Folga");
+
+    private final String descricao;
+
+    TipoOcorrencia(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    @Override
+    public String toString() {
+        return descricao;
+    }
 }
